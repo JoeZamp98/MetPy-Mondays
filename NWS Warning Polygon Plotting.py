544b2cddddbcf30b7ac9831738caee168f04696d
Joe Zampitella <joezamp98@gmail.com>
2022-01-05 19:33:19 -0500

    Added figure creation code for NWS Polygons

--- NWS Warning Polygon Plotting.py
+++ NWS Warning Polygon Plotting.py
@@ -12,3 +12,14 @@ from shapely.geometry import MultiPolygon, Polygon
 def warning_color(phensig):
     return vtec[phensig]['color']
 
+def make_map(bbox, projection=ccrs.PlateCarree()):
+    fig, ax = plt.subplots(figsize=(20,12),
+        subplot_kw = dict(projection=projection))
+    ax.set_extent(bbox)
+
+    gl = ax.gridlines(draw_labels = True)
+    gl.top_labels = gl.right_labels = False
+    gl.xformatter = LONGITUDE_FORMATTER
+    gl.yformatter = LATITUDE_FORMATTER
+    
+    return fig, ax
